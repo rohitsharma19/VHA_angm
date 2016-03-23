9f2a4889d15f19f4a6b73b7df8febcd32ecb176a
TCS <rohitsharma19@gmail.com>
2016-03-23 16:34:01 +0530

    lead-viewAll updated

--- app/modules/lead/leadCtrl.js
+++ app/modules/lead/leadCtrl.js
@@ -25,65 +25,51 @@
 			/*jshint validthis: true */
 			var vm = this;
 
-			vm.saveLead = function(lead){
+			if($state.current.name === 'home.lead.viewAll'){
+				
+				vm.gridOptions = {};
+				vm.gridOptions.enableHorizontalScrollbar = 2;
+				vm.gridOptions.enableVerticalScrollbar = 2;
+				vm.gridOptions.enableFiltering=true;
+
+				vm.gridOptions.columnDefs = [
+                       { field: 'leadId',
+                         cellTemplate: '<div>' +
+                           '<a ng-click="grid.appScope.actionLead(row,\'view\')" >{{row.entity.leadId}}</a>' +
+                           '</div>'
+                       },
+                       { field: 'creationDate' },
+                       { field: 'compName' },
+                       { field: 'abn' },
+                       { field: 'firstName' },
+                       { field: 'lastName' },
+                       { field: 'eMail' },
+                       { field: 'contactNum' },
+                       { name:  'Actions',
+                         cellTemplate: '<div>' +
+                             '<img ng-click="grid.appScope.actionLead(row,\'edit\')" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/edit-icon.png" alt="edit">' +
+                             ' ' +
+                             '<img ng-click="grid.appScope.deleteLead(row)" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/delete-icon.png" alt="delete">'+
+                             '</div>',
+                         enableFiltering:false
+                    	   }
+                ];
+
+                vm.myData = [{
+				        "creationDate": "22-03-2016",
+				        "compName": "TCS",
+				        "abn": "123",
+				        "firstName": "Rohit",
+				        "lastName" : "Sharma",
+				        "eMail" : "rohit@gmail.com",
+				        "contactNum" : "999971618"
+				    }
+				];
+			}
+
+			/*vm.saveLead = function(lead){
 				console.log("inside save lead method");
 				$state.go("home.opportunityCreation");
-			};
-
-			vm.myData = [
-			    {
-			        "firstName": "Cox",
-			        "lastName": "Carney",
-			        "company": "Enormo",
-			        "employed": true
-			    },
-			    {
-			        "firstName": "Lorraine",
-			        "lastName": "Wise",
-			        "company": "Comveyer",
-			        "employed": false
-			    },
-			    {
-			        "firstName": "Nancy",
-			        "lastName": "Waters",
-			        "company": "Fuelton",
-			        "employed": false
-			    }
-			];
-
+			};*/
 		}
-
-		/*function setUpUiGrid() {
-		
-	    $scope.gridOptions = {};
-		$scope.gridOptions.enableHorizontalScrollbar = 2;
-		$scope.gridOptions.enableVerticalScrollbar = 2;
-		$scope.gridOptions.enableFiltering=true;
-
-		$scope.gridOptions.columnDefs = [
-		                                   { field: 'leadId',
-		                                     cellTemplate: '<div>' +
-		                                       '<a ng-click="grid.appScope.actionLead(row,\'view\')" >{{row.entity.leadId}}</a>' +
-		                                       '</div>'
-		                                   },
-		                                   { field: 'creationDate' },
-		                                   { field: 'compName' },
-		                                   { field: 'abn' },
-		                                   { field: 'firstName' },
-		                                   { field: 'lastName' },
-		                                   { field: 'eMail' },
-		                                   { field: 'contactNum' },
-		                                   { name:  'Actions',
-			                                 cellTemplate: '<div>' +
-			                                     '<img ng-click="grid.appScope.actionLead(row,\'edit\')" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/edit-icon.png" alt="edit">' +
-			                                     ' ' +
-			                                     '<img ng-click="grid.appScope.deleteLead(row)" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/delete-icon.png" alt="delete">'+
-			                                     '</div>',
-			                                 enableFiltering:false
-		                                	   }
-		                            ];
-		}*/
-
-
-
 })();
