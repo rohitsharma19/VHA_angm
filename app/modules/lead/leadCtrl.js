781a81a0fe2e79d3eefb2b234a60c275f5c41538
TCS <rohitsharma19@gmail.com>
2016-03-23 15:51:13 +0530

    Changes done to include various lead stageswq

--- app/modules/lead/leadCtrl.js
+++ app/modules/lead/leadCtrl.js
@@ -30,6 +30,60 @@
 				$state.go("home.opportunityCreation");
 			};
 
+			vm.myData = [
+			    {
+			        "firstName": "Cox",
+			        "lastName": "Carney",
+			        "company": "Enormo",
+			        "employed": true
+			    },
+			    {
+			        "firstName": "Lorraine",
+			        "lastName": "Wise",
+			        "company": "Comveyer",
+			        "employed": false
+			    },
+			    {
+			        "firstName": "Nancy",
+			        "lastName": "Waters",
+			        "company": "Fuelton",
+			        "employed": false
+			    }
+			];
+
 		}
+
+		/*function setUpUiGrid() {
+		
+	    $scope.gridOptions = {};
+		$scope.gridOptions.enableHorizontalScrollbar = 2;
+		$scope.gridOptions.enableVerticalScrollbar = 2;
+		$scope.gridOptions.enableFiltering=true;
+
+		$scope.gridOptions.columnDefs = [
+		                                   { field: 'leadId',
+		                                     cellTemplate: '<div>' +
+		                                       '<a ng-click="grid.appScope.actionLead(row,\'view\')" >{{row.entity.leadId}}</a>' +
+		                                       '</div>'
+		                                   },
+		                                   { field: 'creationDate' },
+		                                   { field: 'compName' },
+		                                   { field: 'abn' },
+		                                   { field: 'firstName' },
+		                                   { field: 'lastName' },
+		                                   { field: 'eMail' },
+		                                   { field: 'contactNum' },
+		                                   { name:  'Actions',
+			                                 cellTemplate: '<div>' +
+			                                     '<img ng-click="grid.appScope.actionLead(row,\'edit\')" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/edit-icon.png" alt="edit">' +
+			                                     ' ' +
+			                                     '<img ng-click="grid.appScope.deleteLead(row)" src="http://icons.iconarchive.com/icons/custom-icon-design/office/16/delete-icon.png" alt="delete">'+
+			                                     '</div>',
+			                                 enableFiltering:false
+		                                	   }
+		                            ];
+		}*/
+
+
 
 })();
--- app/modules/lead/leadRoute.js
+++ app/modules/lead/leadRoute.js
@@ -23,6 +23,36 @@ angular.module('lead')
 				templateUrl: 'app/modules/lead/leadCreation.html',
 				controller: 'LeadCtrl',
 				controllerAs: 'vm'
+			})
+			.state('home.lead.viewAll', {
+				/*url:'/viewAll',*/
+				templateUrl: 'app/modules/lead/lead_viewAll.html',
+				controller: 'LeadCtrl',
+				controllerAs: 'vm'
+			})
+			.state('home.lead.create', {
+				/*url:'/create',*/
+				templateUrl: 'app/modules/lead/lead_create.html',
+				controller: 'LeadCtrl',
+				controllerAs: 'vm'
+			})
+			.state('home.lead.edit', {
+				/*url:'/edit',*/
+				templateUrl: 'app/modules/lead/lead_edit.html',
+				controller: 'LeadCtrl',
+				controllerAs: 'vm'
+			})
+			.state('home.lead.view', {
+				/*url:'/view',*/
+				templateUrl: 'app/modules/lead/lead_view.html',
+				controller: 'LeadCtrl',
+				controllerAs: 'vm'
+			})
+			.state('home.lead.delete', {
+				/*url:'/delete',*/
+				templateUrl: 'app/modules/lead/lead_delete.html',
+				controller: 'LeadCtrl',
+				controllerAs: 'vm'
 			});
 
 		
